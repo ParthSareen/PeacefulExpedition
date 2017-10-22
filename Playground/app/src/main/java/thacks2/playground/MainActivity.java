9aea3f372030c67f2cfc8656f5e14c77c2633de7
cyamonide <cyamonide@gmail.com>
2017-10-22 01:34:50 -0400

    Create overview activity

--- Playground/app/src/main/java/thacks2/playground/MainActivity.java
+++ Playground/app/src/main/java/thacks2/playground/MainActivity.java
@@ -1,6 +1,6 @@
 package thacks2.playground;
 
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,17 +10,27 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     static String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "MainActivity created!");
 
         Button btnTest = (Button)(findViewById(R.id.btnTest));
         btnTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.i(TAG, "Test Button Clicked!");
+                startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "OnStart");
     }
 }
--- Scheduler/app/src/main/java/thacks2/scheduler/MainActivity.java
+++ Scheduler/app/src/main/java/thacks2/scheduler/MainActivity.java
@@ -1,7 +1,10 @@
 package thacks2.scheduler;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout welcomeLayout = (ConstraintLayout)findViewById(R.id.welcomeLayout);
+        welcomeLayout.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, OverviewActivity.class);
+                    startActivity(intent);
+                }
+            }
+        );
     }
 }
